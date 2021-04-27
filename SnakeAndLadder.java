a990444999e0bd56919e013bb46a51751bad7f0a
akash <akash@MSI>
2021-04-27 22:19:28 +0530

    Single player at position 0

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -6,6 +6,9 @@
 		public static void main(String[] args) 
 		{
 			System.out.println("Welcome to SnakeAndLadder program");
+			
+			int position=0;
+			System.out.println("Player is at position : "+position);
 		}
 	} 
 
